372dffc7a0192823b2f11dffe57f0b8c429f0db2
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-10 13:14:28 +0600

    hW

--- Rugby/main.js
+++ Rugby/main.js
@@ -1,1 +1,10 @@
 import {Player} from "player.js";
+
+
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth - 15;
+    canvas.height = window. innerHeight - 15;
+
+})
