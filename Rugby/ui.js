f1c9dee988f349354cfe2855b90953dc330422ef
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-10 19:02:39 +0600

    Update ui.js

--- Rugby/ui.js
+++ Rugby/ui.js
@@ -11,18 +11,16 @@ export class UI {
         context.textAlign = 'left';
         context.fillStyle = 'black';
         //first player
-        context.fillText( `Player ${this.game.playerOne}`, 20, 50);
-        // context.textAlign = 'center';
-       //score
-       context.fillText('Score: ' + this.game.score, 20, 80);
+        context.fillText( `Player ${this.game.playerOne}`, 20, 35);
+       // first player score
+       context.fillText('Score: ' + this.game.score, 20, 60);
        //second player
-       context.fillText( `Player ${this.game.playerTwo}`, 640, 50);
-       // context.textAlign = 'center';
-      //score
-      context.fillText('Score: ' + this.game.score, 640, 80);
+       context.fillText( `Player ${this.game.playerTwo}`, 640, 35);
+       // second player score
+       context.fillText('Score: ' + this.game.score, 640, 60);
        //timers
        context.font = this.fontSize* 0.8 + 'px ' + this.fontFamily;
-       context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 320, 50);
+       context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 320, 35);
        //game over message
         
         context.restore();
