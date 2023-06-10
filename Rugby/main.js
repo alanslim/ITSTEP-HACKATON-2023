4c4df5a0fb19006328fd9b4b6d67bc3563b7775b
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-10 13:28:27 +0600

    hW

--- Rugby/main.js
+++ Rugby/main.js
@@ -7,9 +7,38 @@ import * as PIXI from 'pixi.js';
 window.addEventListener('load', () => {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth - 15;
+    canvas.width = window.offset;
     canvas.height = window. innerHeight - 15;
 
+    class Game {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+        }
+
+        update(deltaTime) {
+            this.time += deltaTime;
+    
+        }
+
+        draw(context) {
+            
+        }
+    }
+
+
+    const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
+
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        //console.log(deltaTime);
+        lastTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas. height);
+        game.update(deltaTime);
+        game.draw(ctx);
+        if (!game.gameOver) requestAnimationFrame(animate) ;
+    }
+    animate(0);  
+
 })
-
-
