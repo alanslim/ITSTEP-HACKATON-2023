28a769a597f335dc75b6b64cda28eb7b5fc7c1c2
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-10 13:38:41 +0600

    Update layer.js

--- Rugby/layer.js
+++ Rugby/layer.js
@@ -1,16 +1,53 @@
-export class Layer {
-    constructor (canvas) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext('2d');
-        this.objects = []; 
+class Layer {
+    constructor(game, width, height, speedModifier, image){
+        this.game = game;
+        this.width = width;
+        this.height = height;
+        //нужен потому что каждый бгрнд будет двигаться со своей скоростью
+        this.speedModifier = speedModifier; 
+        this.image = image;
+        this.x = 0;
+        this.y = 0;
     }
+    update() {
+        //В случае если движение вышло из брнда, картинка 
+        //возвращалась в значение 0, чтобы сделать имитацию бесконечного бэка
+        if(this.x < -this.width) this.x = 0;
+        //каждый бэк даижется со скоростью игры*свою скорость, чтобы сделать Paralax эффект
+        else this.x -= this.game.speed * this.speedModifier;
 
-    add (object) {
-        this.objects.push(object);
-        return object;
-    };
+    }
+    draw(context) {
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    }
+}
 
-    remove (object) {
-        const { [Object.keys(obj).pop()]: lastKey, ...newObj } = obj;
-    };
+export class Background {
+    constructor(game) {
+        this.game = game;
+        this.width = 1667;
+        this.height = 500;
+        this.layer1image = document.getElementById('layer1');
+        this.layer2image = document.getElementById('layer2');
+        this.layer3image = document.getElementById('layer3');
+        this.layer4image = document.getElementById('layer4');
+        this.layer5image = document.getElementById('layer5');
+        this.layer1 = new Layer(this.game, this.width, this.height, 0, this.layer1image);
+        this.layer2 = new Layer(this.game, this.width, this.height, 0.2, this.layer2image);
+        this.layer3 = new Layer(this.game, this.width, this.height, 0.4, this.layer3image);
+        this.layer4 = new Layer(this.game, this.width, this.height, 0.8, this.layer4image);
+        this.layer5 = new Layer(this.game, this.width, this.height, 1, this.layer5image);
+        this.backgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5];
+    }
+    update() {
+        this.backgroundLayers.forEach(layer => {
+            layer.update();
+        })
+    }
+    draw(context) {
+        this.backgroundLayers.forEach( layer => {
+            layer.draw(context);
+        })
+    }
 }
