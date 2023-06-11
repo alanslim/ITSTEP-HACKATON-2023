b7ef1111bc9ee2acb3d9857e5456d22146e11afb
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-11 19:19:38 +0600

    Update ui.js

--- Rugby/ui.js
+++ Rugby/ui.js
@@ -14,11 +14,11 @@ export class UI {
         //first player
         context.fillText( `Player ${this.game.playerOne}`, 20, 35);
        // first player score
-       context.fillText('Score: ' + this.game.score, 20, 60);
+       context.fillText('Score: ' + this.game.scoreOne, 20, 60);
        //second player
        context.fillText( `Player ${this.game.playerTwo}`, 640, 35);
        // second player score
-       context.fillText('Score: ' + this.game.score, 640, 60);
+       context.fillText('Score: ' + this.game.scoreTwo, 640, 60);
        //timers
        context.font = this.fontSize* 0.8 + 'px ' + this.fontFamily;
        context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 320, 35);
